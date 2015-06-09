9720952e0feadc12dd1fc6cf6ea165c58bcaab57
Wes Turner <wes@wrd.nu>
2015-06-09 13:28:25 -0600

    Update documentation

--- _static/js/local.js
+++ _static/js/local.js
@@ -26,3 +26,82 @@ $(document).ready(function() {
   });
 })
 // </affix-sidenav>
+/**
+ * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
+ * Licensed under MIT
+ * @author Ariel Flesler
+ * @version 2.1.0
+ */
+;(function(l){'use strict';l(['jquery'],function($){var k=$.scrollTo=function(a,b,c){return $(window).scrollTo(a,b,c)};k.defaults={axis:'xy',duration:0,limit:true};function isWin(a){return!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!==-1}$.fn.scrollTo=function(f,g,h){if(typeof g==='object'){h=g;g=0}if(typeof h==='function'){h={onAfter:h}}if(f==='max'){f=9e9}h=$.extend({},k.defaults,h);g=g||h.duration;var j=h.queue&&h.axis.length>1;if(j){g/=2}h.offset=both(h.offset);h.over=both(h.over);return this.each(function(){if(f===null)return;var d=isWin(this),elem=d?this.contentWindow||window:this,$elem=$(elem),targ=f,attr={},toff;switch(typeof targ){case'number':case'string':if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=d?$(targ):$(targ,elem);if(!targ.length)return;case'object':if(targ.is||targ.style){toff=(targ=$(targ)).offset()}}var e=$.isFunction(h.offset)&&h.offset(elem,targ)||h.offset;$.each(h.axis.split(''),function(i,a){var b=a==='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,prev=$elem[key](),max=k.max(elem,a);if(toff){attr[key]=toff[pos]+(d?0:prev-$elem.offset()[pos]);if(h.margin){attr[key]-=parseInt(targ.css('margin'+b),10)||0;attr[key]-=parseInt(targ.css('border'+b+'Width'),10)||0}attr[key]+=e[pos]||0;if(h.over[pos]){attr[key]+=targ[a==='x'?'width':'height']()*h.over[pos]}}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)==='%'?parseFloat(c)/100*max:c}if(h.limit&&/^\d+$/.test(attr[key])){attr[key]=attr[key]<=0?0:Math.min(attr[key],max)}if(!i&&h.axis.length>1){if(prev===attr[key]){attr={}}else if(j){animate(h.onAfterFirst);attr={}}}});animate(h.onAfter);function animate(a){var b=$.extend({},h,{queue:true,duration:g,complete:a&&function(){a.call(elem,targ,h)}});$elem.animate(attr,b)}})};k.max=function(a,b){var c=b==='x'?'Width':'Height',scroll='scroll'+c;if(!isWin(a))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,doc=a.ownerDocument||a.document,html=doc.documentElement,body=doc.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return $.isFunction(a)||$.isPlainObject(a)?a:{top:a,left:a}}$.Tween.propHooks.scrollLeft=$.Tween.propHooks.scrollTop={get:function(t){return $(t.elem)[t.prop]()},set:function(t){var a=this.get(t);if(t.options.interrupt&&t._last&&t._last!==a){return $(t.elem).stop()}var b=Math.round(t.now);if(a!==b){$(t.elem)[t.prop](b);t._last=this.get(t)}}};return k})}(typeof define==='function'&&define.amd?define:function(a,b){'use strict';if(typeof module!=='undefined'&&module.exports){module.exports=b(require('jquery'))}else{b(jQuery)}}));
+"use strict";
+
+function navbar_scrollto(node) {
+    var sidebar = $('#sidebar-wrapper').first()[0];
+    var navbar = $(sidebar).find('div.sidebar').first()[0];
+
+    if (sidebar && navbar) {
+        var do_scroll = false;
+        if ($(navbar).is(':visible')) {
+            if ($.scrollTo) {
+                do_scroll = true;
+            } else {
+                console.log('$.scrollTo not found');
+            }
+        }
+        if (do_scroll) {
+            // console.log("scrollTo", node);
+            $(sidebar).scrollTo($(node));
+        }
+    } else {
+        console.log('sidebar is', sidebar);
+        console.log('navbar is', navbar);
+    }
+}
+function navbar_update(nodeurl) {
+    var navbar = $('#sidebar-wrapper').find('div.sidebar').first()[0];
+    $(navbar).find('a.youarehere').removeClass('youarehere');
+
+    var content = $('#content-wrapper');
+    ($(content)
+        .find('a.headerlink.youarehere')
+        .removeClass('youarehere')
+        .text('¶')
+    );
+    if (nodeurl) {
+        var navbarlink = $(navbar).find('a[href="' + nodeurl + '"]');
+        ($(content)
+            .find('a[href="' + nodeurl + '"]')
+            .addClass('youarehere')
+            .text('⬅')
+        );
+        if (navbarlink) {
+            navbarlink.addClass('youarehere');
+            navbar_scrollto(navbarlink); // # navbar a.youarehere
+        } else {
+            console.log("nodeurl not found");
+            console.log(nodeurl);
+        }
+    } else {
+        console.log("nodeurl is false");
+        console.log(nodeurl);
+    }
+}
+
+function navbar_init() {
+    // require('jquery.scrollto')
+    //var scriptstr = '<script src="https://cdn.jsdelivr.net/jquery.scrollto/2.1.0/jquery.scrollTo.min.js"></script>';
+    //$(scriptstr).appendTo("head");
+    navbar_update(window.location.hash);
+
+    window.onhashchange = function(e) {
+        // console.log(e); // e.newURL , e.oldURL
+        var loc_hash_url = window.location.hash;
+        console.log(loc_hash_url);
+        if (loc_hash_url != false) {
+            navbar_update(loc_hash_url);
+        };
+    };
+}
+
+
+$(document).ready(navbar_init);
---
+++ _static/js/sidenav-scrollto.js
@@ -0,0 +1,72 @@
+"use strict";
+
+function navbar_scrollto(node) {
+    var sidebar = $('#sidebar-wrapper').first()[0];
+    var navbar = $(sidebar).find('div.sidebar').first()[0];
+
+    if (sidebar && navbar) {
+        var do_scroll = false;
+        if ($(navbar).is(':visible')) {
+            if ($.scrollTo) {
+                do_scroll = true;
+            } else {
+                console.log('$.scrollTo not found');
+            }
+        }
+        if (do_scroll) {
+            // console.log("scrollTo", node);
+            $(sidebar).scrollTo($(node));
+        }
+    } else {
+        console.log('sidebar is', sidebar);
+        console.log('navbar is', navbar);
+    }
+}
+function navbar_update(nodeurl) {
+    var navbar = $('#sidebar-wrapper').find('div.sidebar').first()[0];
+    $(navbar).find('a.youarehere').removeClass('youarehere');
+
+    var content = $('#content-wrapper');
+    ($(content)
+        .find('a.headerlink.youarehere')
+        .removeClass('youarehere')
+        .text('¶')
+    );
+    if (nodeurl) {
+        var navbarlink = $(navbar).find('a[href="' + nodeurl + '"]');
+        ($(content)
+            .find('a[href="' + nodeurl + '"]')
+            .addClass('youarehere')
+            .text('⬅')
+        );
+        if (navbarlink) {
+            navbarlink.addClass('youarehere');
+            navbar_scrollto(navbarlink); // # navbar a.youarehere
+        } else {
+            console.log("nodeurl not found");
+            console.log(nodeurl);
+        }
+    } else {
+        console.log("nodeurl is false");
+        console.log(nodeurl);
+    }
+}
+
+function navbar_init() {
+    // require('jquery.scrollto')
+    //var scriptstr = '<script src="https://cdn.jsdelivr.net/jquery.scrollto/2.1.0/jquery.scrollTo.min.js"></script>';
+    //$(scriptstr).appendTo("head");
+    navbar_update(window.location.hash);
+
+    window.onhashchange = function(e) {
+        // console.log(e); // e.newURL , e.oldURL
+        var loc_hash_url = window.location.hash;
+        console.log(loc_hash_url);
+        if (loc_hash_url != false) {
+            navbar_update(loc_hash_url);
+        };
+    };
+}
+
+
+$(document).ready(navbar_init);
