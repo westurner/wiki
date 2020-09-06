f4b1efdf7ddfce08c32c523cf68cc17730dc59b2
Wes Turner <wes@wrd.nu>
2020-09-05 23:50:04 -0400

    REF: conf.py,reqs.txt: wrd_sphinx_theme

--- conf.py
+++ conf.py
@@ -72,6 +72,8 @@ extensions = [
     #'sphinxcontrib.ansi',
     #'sphinxcontrib.programoutput',
     #'sphinxcontrib.issuetracker',
+    'sphinxjp.themes.basicstrap',
+    'wrd_sphinx_theme',
     'sphinxcontrib.srclinks'
 ]
 
@@ -149,7 +151,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'basicstrap'
+html_theme = 'wrd_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
